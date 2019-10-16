9367856e60c6bd82ae16d1be74ab53876c27d1f7
Leon <s3235319@student.rmit.edu.au>
2019-10-16 19:41:21 +1100

    basic login page and rows for video display

--- login.php
+++ login.php
@@ -23,45 +23,16 @@
 		</div>
 	</div>
 	<div class="container">
-		<form action="" method="GET">
-			<div class="input-group">
-				<input type="text" name="searchBar" id="searchBar" placeholder="Search" autocomplete="off" class="form-control">
-				<input type="hidden" name="searchInput" id="searchInput">
-				<div class="input-group-append">
-					<button type="submit" class="btn btn-primary" id="search">Search</button>
-				</div>
+		<form action="search.php" method="POST" class="mb-5">
+			<div class="form-group">
+				<input type="text" name="username" id="username" placeholder="Username" autocomplete="off" class="form-control">
+			</div>
+			<div class="form-group">
+				<input type="text" name="password" id="password" placeholder="Password" autocomplete="off" class="form-control">
+			</div>
+			<button class="btn btn-primary">Login</button>
 			</div>
 		</form>
 	</div>
-	<script type="text/javascript">
-		document.getElementById('search').addEventListener("click",searchVideo);
-
-		function searchVideo(){
-			var searchItem = document.getElementById('searchBar').value;
-			document.getElementById('searchInput').value = searchItem;
-		}
-	</script>
-<?php 
-	$keyword='';
-	if(isset($_GET['searchInput'])){
-		$keyword = $_GET['searchInput'];
-	}
-	if(!empty($keyword)){
-		$API_key = 'AIzaSyC-bz33drTNC_vAe7YB1LpdzFHivbYTk7Y';
-		$maxResults = 10;
-		$videoList = json_decode(file_get_contents('https://www.googleapis.com/youtube/v3/search?order=viewCount&part=snippet&q='.$keyword.'&maxResults='.$maxResults.'&key='.$API_key));
-
-		foreach($videoList->items as $item){
-			if(isset($item->id->videoId)){
-							$viewCount = json_decode(file_get_contents('https://www.googleapis.com/youtube/v3/videos?part=statistics&id=' .$item->id->videoId.'&key='.$API_key.''));
-			echo '<div class="youtube-video">
-			<iframe width="280" height="150" src="https://www.youtube.com/embed/'.$item->id->videoId.'" frameborder="0" allowfullscreen></iframe>
-			<h2>'. $item->snippet->title .'</h2>
-			<h1>'.$viewCount->items[0]->statistics->viewCount.'</h1><h1>'.$viewCount->items[0]->statistics->likeCount.'</h1>
-			</div>';
-			}
-	}
-}
-?>
 </body>
 </html>
